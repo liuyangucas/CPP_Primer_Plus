d6ea163d80874a2faf87f99083ab5a50b4e5533f
liuyangucas <31404103+liuyangucas@users.noreply.github.com>
2020-02-17 17:05:17 +0800

    Create test7.cpp

---
+++ chapter10/test7/test7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"plorg.h"
+using namespace std;
+int main() {
+	Plorg plg;
+	plg.Show();
+	plg.SetCI(100);
+	plg.Show();
+	plg.NewPLG("emmmmmmm");
+	plg.Show();
+	return 0;
+}
